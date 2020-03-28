4b12a6af40c0461b87aabb506326438c9f222d27
Toni-Ty <Antonique2479@yahoo.com>
2020-03-28 18:17:06 -0500

    complete math quiz

---
+++ JavaScript_Basics_Assessment/activity-1.js
@@ -0,0 +1,49 @@
+// The product of 9 and 9
+
+let num1 = 9;
+let num2 = 9;
+var product = num1 * num2;
+console.log(product);
+product = 81
+
+// The quotient of 56 and 12
+
+
+let num1 = 56;
+let num2 = 12;
+var quotient = num1 / num2;
+console.log(quotient);
+quotient = 4.666666666666667
+
+// The remainder of 281 divided by 9
+
+let num1 = 281;
+let num2 = 9;
+var remainder = num1 % num2;
+console.log(remainder);
+remainder = 2
+
+// 18 divided by 0
+
+let num1 = 18;
+let num2 = 0;
+var result = num1 / num2;
+console.log(result);
+result = infinity
+
+// The sum of string 56 and string 92
+
+
+let string1 = '56';
+let string2 = '92';
+var result = string1 + string2;
+console.log(result);
+result = 5692
+
+// Using any comparison operator you wish on the numbers 42 and 52, make it evaluate to false.
+
+let num1 = 42;
+let num2 = 52;                   
+answer = (num1 + num2 === 0);
+console.log(answer);
+answer = false
