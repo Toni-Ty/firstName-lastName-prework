e2c45ed5be302339b5b29023467f1389a3158ae6
Toni-Ty <Antonique2479@yahoo.com>
2020-03-28 19:13:33 -0500

    activity 2 complete

---
+++ JavaScript_Basics_Assessment/activity-2.js
@@ -0,0 +1,13 @@
+// Grab a user input using a prompt() and store it in a variable.
+// Add a conditional statement where if the variable's length is greater than 4, we alert the user that their name is greater than four characters.
+// Otherwise, alert that their name is less than four characters.
+
+
+const name = prompt('What is your name?');
+
+if (name.length > 4) {
+    alert('Your name is greater than 4 characters');
+} else {
+    alert('Your name is less than 4 characters');
+    }
+
