39a857cd4aa35f72b3a43087004328bbbde3f34c
Toni-Ty <Antonique2479@yahoo.com>
2020-03-28 20:00:24 -0500

    complete activity 3

---
+++ JavaScript_Basics_Assessment/activity-3.js
@@ -0,0 +1,16 @@
+// Start off by creating an array with three student names.
+// Create a loop that will prompt the user for three more names.
+// After every user input, store the new name into the array.
+// Create a new loop that will iterate through the array and console log each element of the array.
+
+const studentNames = ['Linda','Ava','Corey'];
+
+for (let i = 0; i < 3; i++) {
+const name = prompt('Please enter a name');
+ studentNames.push(name);
+}
+
+for (let i = 0; i < studentNames.length; i++) {
+  const names = studentNames[i];
+  console.log(names)
+}
